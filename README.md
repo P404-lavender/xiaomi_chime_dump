2f1ea7d69aba48fabcbdda3579440f8d3e01691d
xenxynon <flynryder427@gmail.com>
2022-10-07 16:12:00 +0000

    Add extras for lineage_chime-userdebug 13 TP1A.220905.004.A2 eng.a.20221006.095930 test-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## lineage_chime-userdebug 13 TP1A.220905.004.A2 eng.a.20221006.095930 test-keys
+- Manufacturer: xiaomi
+- Platform: bengal
+- Codename: chime
+- Brand: Xiaomi
+- Flavor: lineage_chime-userdebug
+- Release Version: 13
+- Id: TP1A.220905.004.A2
+- Incremental: eng.a.20221006.095930
+- Tags: test-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: Xiaomi/lineage_chime/chime:13/TP1A.220905.004.A2/a10060957:userdebug/test-keys
+- OTA version: 
+- Branch: lineage_chime-userdebug-13-TP1A.220905.004.A2-eng.a.20221006.095930-test-keys
+- Repo: xiaomi_chime_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat product/app/webview/webview.apk.* 2>/dev/null >> product/app/webview/webview.apk
+rm -f product/app/webview/webview.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null >> vendor/lib64/camera/components/com.qti.node.mialgocontrol.so
+rm -f vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._Bengal_SoC.dtb.* 2>/dev/null >> bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._Bengal_SoC.dtb
+rm -f bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._Bengal_SoC.dtb.* 2>/dev/null
